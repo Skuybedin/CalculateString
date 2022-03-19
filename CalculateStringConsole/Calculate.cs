7d2e8b293d0b0bf69243d42bc534f0d646158f23
Nikita <skuybedin.nik@yandex.ru>
2022-03-19 16:12:59 +0300

    v1.0

---
+++ CalculateStringConsole/Calculate.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateStringConsole
+{
+    internal interface ICalculate
+    {
+        string InputValue { get; set; }
+        string GetLanguage();
+        int CalcWordCount();
+        int CalcVowelCount();
+    }
+
+    public class CalculateString : ICalculate
+    {
+        public string InputValue { get; set; }
+        private static readonly string VowelsRu = "аоэеиыуёюя";
+        private static readonly string VowelsEn = "aeiouy";
+        private static readonly string RusLang = "йцукенгшщзхъфывапролджэячсмитьбюё";
+        private static readonly string EngLang = "qwertyuiopasdfghjklzxcvbnm";
+
+        public CalculateString(string InputValue)
+        {
+            if (InputValue == null || InputValue == string.Empty) throw new ArgumentNullException("input can not be empty or null");
+            else this.InputValue = InputValue.Trim();
+
+        }
+
+        public string GetLanguage()
+        {
+            foreach (var letter in InputValue.ToLower())
+            {
+                if (EngLang.Contains(letter)) return "English";
+                else if (RusLang.Contains(letter)) return "Русский";
+            }
+
+            return new Exception("Sorry, we support only Russian and English languages").Message;
+        }
+
+        public int CalcWordCount()
+        {
+            List<string> wordList = InputValue.Split(' ').ToList();
+            int count = 0;
+            foreach (var item in wordList)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (RusLang.Contains(item[i].ToString().ToLower()) || EngLang.Contains(item[i].ToString().ToLower()))
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public int CalcVowelCount()
+        {
+            int count = 0;
+
+            if (GetLanguage() == "English")
+            {
+                foreach (var letter in InputValue.ToLower())
+                {
+                    if (VowelsEn.Contains(letter)) count++;
+                }
+            }
+            else if (GetLanguage() == "Русский")
+            {
+                foreach (var letter in InputValue.ToLower())
+                {
+                    if (VowelsRu.Contains(letter)) count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ CalculateStringConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculateStringConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalculate calc = new CalculateString("Привет, как дела? !!");
+            Console.WriteLine($"Количество слов в предложении: {calc.CalcWordCount()}");
+            Console.WriteLine($"Количество гласных в предложении: {calc.CalcVowelCount()}");
+            Console.WriteLine($"Язык: {calc.GetLanguage()}");
+
+            Console.ReadLine();
+        }
+    }
+}
